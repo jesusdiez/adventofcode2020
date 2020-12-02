c8826711cc7f8e9b19ce1bc20f9d028f0c8d5e82
Jesús Díez <jesus@jesusdiez.com>
2020-12-02 15:33:49 +0100

    feat(day2): Part 1 resolved

--- Day2/Day2.php
+++ Day2/Day2.php
@@ -3,12 +3,29 @@ declare(strict_types=1);
 
 namespace AoC\Day1;
 
-require_once(__DIR__ . '/../vendor/autoload.php');
-
 final class Day2
 {
     public static function main(): void
     {
+        $lines = file(__DIR__ . '/input');
+        $rules = array_map(fn($line) => self::match('/(\d+)-(\d+) ([a-z]): (\w+)/', $line), $lines);
+        $valid = 0;
+        foreach ($rules as [$_, $min, $max, $letter, $password]) {
+            $letterMap = \array_count_values(\str_split($password));
+            if (key_exists($letter, $letterMap) && $letterMap[$letter] >= $min && $letterMap[$letter] <= $max) {
+                $valid++;
+            }
+        }
 
+        echo "Valid: " . $valid;
+    }
+
+    private static function match(string $pattern, string $haystack): array
+    {
+        preg_match($pattern, $haystack, $matches);
+
+        return $matches;
     }
 }
+
+Day2::main();
