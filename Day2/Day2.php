9cdc67f7194dc9144131901cc721d0677badc5d3
Jesús Díez <jesus@jesusdiez.com>
2020-12-02 16:07:38 +0100

    feat(day2): Part 2 resolved

--- Day2/Day2.php
+++ Day2/Day2.php
@@ -5,19 +5,36 @@ namespace AoC\Day1;
 
 final class Day2
 {
-    public static function main(): void
+    public static function main1(): void
     {
-        $lines = file(__DIR__ . '/input');
-        $rules = array_map(fn($line) => self::match('/(\d+)-(\d+) ([a-z]): (\w+)/', $line), $lines);
         $valid = 0;
-        foreach ($rules as [$_, $min, $max, $letter, $password]) {
+        foreach (self::parseInput() as [$_, $min, $max, $letter, $password]) {
             $letterMap = \array_count_values(\str_split($password));
             if (key_exists($letter, $letterMap) && $letterMap[$letter] >= $min && $letterMap[$letter] <= $max) {
                 $valid++;
             }
         }
 
-        echo "Valid: " . $valid;
+        echo "Part1 Valid: " . $valid . PHP_EOL;
+    }
+
+    public static function main2(): void
+    {
+        $valid = 0;
+        foreach (self::parseInput() as [$_, $pos1, $pos2, $letter, $password]) {
+            if ((($password[$pos1 - 1] === $letter ? 1 : 0) + ($password[$pos2 - 1] === $letter ? 1 : 0 )) == 1) {
+                $valid++;
+            }
+        }
+
+        echo "Part2 Valid: " . $valid . PHP_EOL;
+    }
+
+    protected static function parseInput(): array
+    {
+        $lines = file(__DIR__ . '/input');
+
+        return array_map(fn($line) => self::match('/(\d+)-(\d+) ([a-z]): (\w+)/', $line), $lines);
     }
 
     private static function match(string $pattern, string $haystack): array
@@ -28,4 +45,5 @@ final class Day2
     }
 }
 
-Day2::main();
+Day2::main1();
+Day2::main2();
