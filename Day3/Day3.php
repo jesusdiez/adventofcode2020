3234901eea46e31c4fa74477b1dc7fae9fe178e7
Jesús Díez <jesus@jesusdiez.com>
2020-12-04 18:00:32 +0100

    feat(day3): Part 2

--- Day3/Day3.php
+++ Day3/Day3.php
@@ -9,33 +9,54 @@ final class Day3
 
     public static function main1(): void
     {
-        $speedX = 3;
         $map = self::parseInput();
+        $treeCount = self::treeCount($map, 3,1);
+
+        printf("Part 1.\nTree count: %d\n", $treeCount);
+    }
+
+    public static function main2(): void
+    {
+        $map = self::parseInput();
+        $slopes = [
+            [1,1],
+            [3,1],
+            [5,1],
+            [7,1],
+            [1,2],
+        ];
+        $results = array_map(
+            fn($slope) => self::treeCount($map, $slope[0], $slope[1]),
+            $slopes
+        );
+
+        printf("Part 2.\nResults: %s.\nTotal Trees: %d\n", json_encode($results), array_product($results));
+    }
+
+    private static function treeCount(array $map, int $speedX, int $speedY): int
+    {
         $height = count($map);
         $width = count(reset($map));
 
         $x = 0;
         $treeCount = 0;
-        for ($y=0; $y<$height-1; $y++) {
+        for ($y = 0; $y < $height - 1; $y += $speedY) {
             $x += $speedX;
             if ($x > $width - 1) {
                 $x = $x % $width;
             }
-            $currentPos = $map[$y+1][$x];
+            $currentPos = $map[$y + $speedY][$x];
             $treeCount += $currentPos == self::TREE ? 1 : 0;
         }
 
-        printf("Tree count: %d\n", $treeCount);
+        return $treeCount;
     }
 
-    public static function main2(): void
-    {
-    }
-
-    protected static function parseInput(): array
+    private static function parseInput(): array
     {
         return array_map(fn($line) => str_split(trim($line)), file(__DIR__ . '/input'));
     }
 }
 
 Day3::main1();
+Day3::main2();
