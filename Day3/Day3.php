2414503a747030ad14ddab5791fd0dd2bab386e7
Jesús Díez <jesus@jesusdiez.com>
2020-12-04 11:17:31 +0100

    chore(day3): Setup

---
+++ Day3/Day3.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace AoC\Day3;
+
+final class Day3
+{
+    public static function main1(): void
+    {
+    }
+
+    public static function main2(): void
+    {
+    }
+}
+
+Day3::main1();
+Day3::main2();
