b8b4045ceb6d0c8062e09295e9bb11273e79f334
Jesús Díez <jesus@jesusdiez.com>
2020-12-04 17:40:46 +0100

    feat(day3): Part 1

--- Day3/Day3.php
+++ Day3/Day3.php
@@ -5,14 +5,37 @@ namespace AoC\Day3;
 
 final class Day3
 {
+    private const TREE = '#';
+
     public static function main1(): void
     {
+        $speedX = 3;
+        $map = self::parseInput();
+        $height = count($map);
+        $width = count(reset($map));
+
+        $x = 0;
+        $treeCount = 0;
+        for ($y=0; $y<$height-1; $y++) {
+            $x += $speedX;
+            if ($x > $width - 1) {
+                $x = $x % $width;
+            }
+            $currentPos = $map[$y+1][$x];
+            $treeCount += $currentPos == self::TREE ? 1 : 0;
+        }
+
+        printf("Tree count: %d\n", $treeCount);
     }
 
     public static function main2(): void
     {
     }
+
+    protected static function parseInput(): array
+    {
+        return array_map(fn($line) => str_split(trim($line)), file(__DIR__ . '/input'));
+    }
 }
 
 Day3::main1();
-Day3::main2();
