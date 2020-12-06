9518438d4785aa98fe2ae3a6662519971f555eed
Jesús Díez <jesus@jesusdiez.com>
2020-12-06 19:44:27 +0100

    feat(day6): Part 1

--- Day6/Day6.php
+++ Day6/Day6.php
@@ -7,21 +7,55 @@ use AoC20\Tools\Day;
 
 final class Day6 implements Day
 {
-    private array $groups;
+    private array $groupAnswers;
 
     public function __construct(string $inputContent)
     {
-        $this->groups = self::parseInput($inputContent);
+        $this->groupAnswers = self::parseInput($inputContent);
+//        echo json_encode($this->groupAnswers, \JSON_PRETTY_PRINT);
+//        die;
     }
 
     public static function parseInput(string $contents): array
+    {
+        return array_map(
+            fn(string $groupStr):array => array_filter(explode(PHP_EOL, $groupStr)),
+            self::splitInputStrInArrayOfGroups($contents)
+        );
+    }
+
+    public static function mapGroupAnswers(array $groupAnswers): array
+    {
+        $ar = array_reduce(
+            $groupAnswers,
+            fn($carry, $personAnswer) => array_merge($carry, str_split($personAnswer)),
+            []
+        );
+
+        echo "\nGroup Answers: ".json_encode($groupAnswers);
+        echo "\nMerged: ".json_encode($ar);
+        $ar = array_values(array_unique($ar));
+        sort($ar);
+        echo "\nUnique sort: ".json_encode($ar);
+        echo "\nCount: ".count($ar);
+        echo "\n";
+
+        return array_values($ar);
+    }
+
+    public static function splitInputStrInArrayOfGroups(string $contents): array
     {
         return explode(PHP_EOL . PHP_EOL, $contents);
     }
 
     public function part1(): int
     {
-        return -1;
+        $counts = array_map(
+                fn($groupAnswers) => count(self::mapGroupAnswers($groupAnswers)),
+                $this->groupAnswers
+            );
+
+        return array_sum($counts);
     }
 
     public function part2(): int
--- Day6/Tests/Day6Test.php
+++ Day6/Tests/Day6Test.php
@@ -6,6 +6,16 @@ use PHPUnit\Framework\TestCase;
 
 class Day6Test extends TestCase
 {
+    public function testPart1Simple()
+    {
+        $sample = <<<EOF
+abcx
+abcy
+abcz
+EOF;
+        self::assertEquals(6, (new Day6($sample))->part1());
+    }
+
     public function testPart1()
     {
         $sample = <<<EOF
@@ -25,8 +35,23 @@ a
 
 b
 EOF;
-        $sut = new Day6($sample);
+        self::assertEquals(11, (new Day6($sample))->part1());
+    }
 
-        self::assertEquals(11, $sut->part1());
+    public function testMapPersonAnswer(): void
+    {
+        $input = <<<EOF
+a
+
+b
+
+c
+EOF;
+        self::assertEquals(['a','b','c'], Day6::splitInputStrInArrayOfGroups($input));
+    }
+
+    public function testMapGroupAnswers(): void
+    {
+        self::assertEquals(['a','b','c'], Day6::mapGroupAnswers(['ab','ac']));
     }
 }
