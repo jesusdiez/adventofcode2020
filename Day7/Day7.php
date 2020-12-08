943afbbedc542cf493cad550581b5abe2b5b541f
Jesús Díez <jesus@jesusdiez.com>
2020-12-09 00:43:38 +0100

    feature(day7): Part 1

--- Day7/Day7.php
+++ Day7/Day7.php
@@ -14,14 +14,70 @@ final class Day7 implements Day
         $this->data = self::parseInput($inputContent);
     }
 
+    public static function parseLine(string $line): array
+    {
+        $patternMain = '/([a-z]+ [a-z]+) bags contain (.*)\.$/';
+        $patternSub = '/(\d+) ([a-z]+ [a-z]+) bags?/';
+        $matches = [];
+        \preg_match($patternMain, $line, $matches);
+        $main = $matches[1];
+        $inside = $matches[2] ?? null;
+        if ($inside === null) {
+            return [$main => []];
+        }
+
+        $insideMap = array_reduce(
+            explode(', ', $inside),
+            function ($carry, $bag) use ($patternSub) {
+                if (preg_match($patternSub, $bag, $matches)) {
+                    $carry[$matches[2]] = $matches[1];
+                };
+
+                return $carry;
+            },
+            []
+        );
+
+        return [$main => $insideMap];
+    }
+
     public static function parseInput(string $contents): array
     {
-        return [];
+        return array_reduce(
+            array_filter(explode(PHP_EOL, $contents)),
+            fn(array $carry, string $line) => $carry += self::parseLine($line),
+            []
+        );
+    }
+
+    public function canContain(string $selColor): array
+    {
+        $canContain = array_reduce(
+            array_keys($this->data),
+            function (array $carry, $containerBagColor) use ($selColor) {
+                if (\key_exists($selColor, $this->data[$containerBagColor])) {
+                    array_push($carry, $containerBagColor);
+                }
+
+                return $carry;
+            },
+            []
+        );
+
+        return array_unique(
+            array_reduce(
+                $canContain,
+                function (array $carry, $containerBagColor) {
+                    return array_merge($carry, $this->canContain($containerBagColor));
+                },
+                $canContain
+            )
+        );
     }
 
     public function part1(): int
     {
-        return -1;
+        return count($this->canContain('shiny gold'));
     }
 
     public function part2(): int
@@ -29,3 +85,4 @@ final class Day7 implements Day
         return -1;
     }
 }
+
--- Day7/Tests/Day7Test.php
+++ Day7/Tests/Day7Test.php
@@ -1,8 +1,58 @@
 <?php
 declare(strict_types=1);
 
+use AoC20\Day7\Day7;
 use PHPUnit\Framework\TestCase;
 
 class Day7Test extends TestCase
 {
+    private Day7 $sut;
+    private string $input;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->input = <<<EOF
+light red bags contain 1 bright white bag, 2 muted yellow bags.
+dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+bright white bags contain 1 shiny gold bag.
+muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+faded blue bags contain no other bags.
+dotted black bags contain no other bags.
+EOF;
+        $this->sut = new Day7($this->input);
+    }
+
+    public function testParseLine(): void
+    {
+        $line = "light red bags contain 1 bright white bag, 2 muted yellow bags.";
+
+        self::assertEquals(['light red' => ['bright white' => 1, 'muted yellow' => 2]], Day7::parseLine($line));
+    }
+
+    public function testParseInput(): void
+    {
+        $expected = [
+            'light red' => ['bright white' => 1, 'muted yellow' => 2],
+            'dark orange' => ['bright white' => 3, 'muted yellow' => 4],
+            'bright white' => ['shiny gold' => 1],
+            'muted yellow' => ['shiny gold' => 2, 'faded blue' => 9],
+            'shiny gold' => ['dark olive' => 1, 'vibrant plum' => 2],
+            'dark olive' => ['faded blue' => 3, 'dotted black' => 4],
+            'vibrant plum' => ['faded blue' => 5, 'dotted black' => 6],
+            'faded blue' => [],
+            'dotted black' => [],
+        ];
+
+        self::assertEquals($expected, Day7::parseInput($this->input));
+    }
+
+    public function testCanContain(): void
+    {
+        $expected = ['bright white', 'muted yellow', 'light red', 'dark orange', ];
+        self::assertEquals($expected, $this->sut->canContain('shiny gold'));
+    }
 }
